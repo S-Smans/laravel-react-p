0b597bb30349c8b3ca1e4c5f22c6f77e9c87583b
Sandis <smanssandis@gmail.com>
2024-09-20 20:04:45 +0300

    signup login & axios

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,11 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 function DefaultLayout() {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  const { user, token } = useStateContext();
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  const onLogout = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>Header</div>
+          <div>{user.name}</div>
+          <a href="#" onClick={onLogout} className="btn-logout">
+            Logout
+          </a>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 function GuestLayout() {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  const { token } = useStateContext();
+
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default GuestLayout;
